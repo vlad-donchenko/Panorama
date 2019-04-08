e4553827003ff379e47e989eaf1b2b87691e6e47
Vlad Donchenko <2pi14b.donchenko@gmail.com>
2019-04-09 01:26:32 +0300

    full hd

---
+++ js/common.js
@@ -0,0 +1,39 @@
+
+$('.product-slider').slick({
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  prevArrow: $('.prev-prod'),
+  nextArrow: $('.next-prod'),
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+$(".product-slider").find('.slick-cloned a').removeAttr('data-fancybox');
